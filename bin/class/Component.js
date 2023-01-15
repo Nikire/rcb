86db49252686deffeef7b4abc735ddc50cb61ca5
Erik Pastuszek <erikpastuzek@gmail.com>
2023-01-15 13:35:20 -0300

    Added main structure of templates and creation of files

--- bin/class/Component.js
+++ bin/class/Component.js
@@ -1,13 +1,17 @@
 const Main = require('./Main.js');
-
 class Component extends Main {
-	constructor(name) {
-		super();
-		this.name = name;
-	}
-	componentPath = '/src/components/';
-	createComponent() {
-		this.findOrCreateFolder(this.componentPath + this.name);
-	}
+  constructor(name) {
+    super();
+    this.name = this.capitalizeWord(name);
+  }
+  componentPath = '/src/components/';
+  createComponent() {
+    let path = this.componentPath + this.name;
+    let jsx = path + '/' + this.name + '.jsx';
+    let css = path + '/' + this.name + '.modules.css';
+    this.findOrCreateFolder(path);
+    this.createFile(jsx, this.useTemplate('/components/rfc.js', this.name));
+    this.createFile(css);
+  }
 }
 module.exports = Component;
